18a123a96288801afad5699e50b7f2ba512ea811
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-07-17 23:32:13 -0300

    ⚡  (server) add createRefreshToken func to database

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -1,4 +1,15 @@
 import { RefreshTokensStore, UsersStore } from './types';
+import { v4 as uuid } from 'uuid';
+
 export const users: UsersStore = new Map();
 
 export const tokens: RefreshTokensStore = new Map();
+
+export const createRefreshToken = (email: string) => {
+  const currentUserTokens = tokens.get(email) ?? [];
+  const refreshToken = uuid();
+
+  tokens.set(email, [...currentUserTokens, refreshToken]);
+
+  return refreshToken;
+};
