c1fffb02908b73618da68c1dfeee60a97e10db1c
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-07-18 00:54:41 -0300

    ⚡  (web) add a login form in home page

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,5 +1,46 @@
+import { FormEvent, useCallback, useRef } from 'react';
+
+import { useAuth } from 'hooks/useAuth';
+import { asGuest } from 'auth/asGuest';
+
+import styles from './index.module.css';
+
 const Home = () => {
-  return <h1>Ahhh ... Hellow???</h1>;
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const { signIn } = useAuth();
+
+  const handleOnSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+
+      const form = formRef.current;
+
+      const email = (form[0] as HTMLInputElement).value;
+      const password = (form[1] as HTMLInputElement).value;
+
+      signIn({ email, password });
+    },
+    [signIn]
+  );
+
+  return (
+    <div className={styles.container}>
+      <form ref={formRef} onSubmit={handleOnSubmit} className={styles.form}>
+        <input type="text" className={styles.input} />
+        <input type="password" className={styles.input} />
+        <button type="submit" className={styles.button}>
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps = asGuest(async () => {
+  return {
+    props: {},
+  };
+});
