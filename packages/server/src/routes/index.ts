a98128d5bae4e21290b625a636ee2553642821be
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-07-17 23:46:55 -0300

    ✨  (server) add route to refresh token

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 
-import { users } from '../database';
+import { addUserInfoToRequest } from '../middlewares';
+
+import {
+  checkRefreshTokenIsValid,
+  users,
+  invalidateRefreshToken,
+} from '../database';
 
 import { CreateSessionDTO } from '../types';
 import { generateJwtAndRefreshToken } from '../auth';
@@ -29,6 +35,53 @@ routes.post('/sessions', (request, response) => {
   return response.json({
     token,
     refreshToken,
+    permissions,
+    roles,
+  });
+});
+
+routes.post('/refresh', addUserInfoToRequest, (request, response) => {
+  const email = request.user;
+  const { refreshToken } = request.body;
+
+  const user = users.get(email);
+
+  if (!user) {
+    return response.status(401).json({
+      error: true,
+      message: 'User not found.',
+    });
+  }
+
+  if (!refreshToken) {
+    return response
+      .status(401)
+      .json({ error: true, message: 'Refresh token is required.' });
+  }
+
+  const isValidRefreshToken = checkRefreshTokenIsValid(email, refreshToken);
+
+  if (!isValidRefreshToken) {
+    return response
+      .status(401)
+      .json({ error: true, message: 'Refresh token is invalid.' });
+  }
+
+  invalidateRefreshToken(email, refreshToken);
+
+  const { permissions, roles } = user;
+
+  const { token, refreshToken: newRefreshToken } = generateJwtAndRefreshToken(
+    email,
+    {
+      permissions,
+      roles,
+    }
+  );
+
+  return response.json({
+    token,
+    refreshToken: newRefreshToken,
     permissions,
     roles,
   });
