165dbad3a1042053904cba425626d6b106344dff
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-07-18 00:57:43 -0300

    ✨  (web) add dashboard page

---
+++ packages/web/src/pages/dashboard.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+import { asLogged } from 'auth/asLogged';
+import { useAuth } from 'hooks/useAuth';
+
+import { api } from 'services/api';
+
+const Dashboard = () => {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    api
+      .get('metrics')
+      .then(r => console.log('Dashboard', r.data))
+      .catch(err => console.error('Dashboard error', err));
+  }, []);
+
+  return <h1>Dashboard: {user?.email}</h1>;
+};
+
+export default Dashboard;
+
+export const getServerSideProps = asLogged(async () => {
+  return {
+    props: {},
+  };
+});
