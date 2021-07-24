9491e8a3a9d20903136b390b3008455e63189f41
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-07-24 16:12:38 -0300

    ⚡  (web) refactor handleOnSubmit func in sign page
    Was add yup validation and exception handling

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useRef, useState } from 'react';
 
 import * as C from '@chakra-ui/react';
+import * as Yup from 'yup';
 
 import { Form } from '@unform/web';
-import { FormHandles } from '@unform/core';
+import { FormHandles, SubmitHandler } from '@unform/core';
 
 import { MdEmail as EmailIcon, MdLock as LockIcon } from 'react-icons/md';
 
@@ -12,23 +13,44 @@ import { Input, Logo } from 'components';
 import { useAuth } from 'hooks/useAuth';
 import { asGuest } from 'auth/asGuest';
 
+import { getValidationErrors } from 'utils/getValidationErrors';
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
 const Home = () => {
   const formRef = useRef<FormHandles>(null);
   const { signIn } = useAuth();
 
   const [loading, setLoading] = useState(false);
 
+  const handleOnSubmit: SubmitHandler<FormData> = useCallback(
+    async data => {
+      setLoading(true);
+      formRef.current.setErrors({});
 
-  const handleOnSubmit = useCallback(
-    (e: FormEvent) => {
-      e.preventDefault();
+      try {
+        const schema = Yup.object().shape({
+          email: Yup.string().email().required(),
+          password: Yup.string().required(),
+        });
 
-      const form = formRef.current;
+        await schema.validate(data, { abortEarly: false });
 
-      const email = (form[0] as HTMLInputElement).value;
-      const password = (form[1] as HTMLInputElement).value;
+        const { email, password } = data;
 
-      signIn({ email, password });
+        await signIn({ email, password });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current.setErrors(errors);
+        }
+
+        setLoading(false);
+      }
     },
     [signIn]
   );
