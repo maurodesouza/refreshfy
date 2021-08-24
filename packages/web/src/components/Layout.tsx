bb19e0e117258f33892e303643affa69bd69b729
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-08-24 09:20:03 -0300

    ⚡  (web) add layout component

---
+++ packages/web/src/components/Layout.tsx
@@ -0,0 +1,31 @@
+import * as C from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { Menu } from '.';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  const { asPath } = useRouter();
+
+  const isLoginPage = asPath === '/';
+
+  return isLoginPage ? (
+    <>{children}</>
+  ) : (
+    <C.Center pt={[5, '70px']}>
+      <C.Box w="100%" maxW="600px" h="100%">
+        <C.Box mb="4" p="4" borderRadius="8" boxShadow="xs">
+          <Menu />
+        </C.Box>
+
+        <C.Box borderRadius="8" boxShadow="xs" p="4">
+          {children}
+        </C.Box>
+      </C.Box>
+    </C.Center>
+  );
+};
+
+export { Layout };
--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -6,11 +6,15 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AuthContextProvider } from 'contexts/Auth';
 import { theme } from 'styles/theme/default';
 
+import { Layout } from 'components';
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
       <AuthContextProvider>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </AuthContextProvider>
     </ChakraProvider>
   );
