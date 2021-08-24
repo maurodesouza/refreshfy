881815adfe4899c5d63a20b93313aca71727a940
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-08-24 09:10:35 -0300

    ⚡  (web) change height and add padding x to root elements

--- packages/web/src/styles/theme/default.ts
+++ packages/web/src/styles/theme/default.ts
@@ -3,10 +3,17 @@ import { extendTheme } from '@chakra-ui/react';
 const theme = extendTheme({
   styles: {
     global: {
+      'html, body, #__next': {
+        height: '100%',
+      },
+
+      '#__next': {
+        px: '5',
+      },
       body: {
         bg: 'gray.50',
         color: 'gray.900',
-        px: '5',
+        minHeight: '100vh',
       },
     },
   },
