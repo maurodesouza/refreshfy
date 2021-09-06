b92b91de3dca9e3ab00950ac74e9f4ec51b8dd37
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-09-06 20:41:51 -0300

    ✨  (web) add posts page

--- packages/web/src/pages/posts.tsx
+++ packages/web/src/pages/posts.tsx
@@ -1,5 +1,65 @@
+import { useEffect, useState } from 'react';
+import * as C from '@chakra-ui/react';
+
+import { Pagination } from 'components';
+import { asLogged } from 'auth/asLogged';
+
+import { PostResponseData, PostData } from 'types/api';
+import { api } from 'services/api';
+import { Roles } from 'types';
+
 const Posts = () => {
-  return <h1>Posts</h1>;
+  const [posts, setPosts] = useState<PostData[]>([]);
+
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const loadPosts = async () => {
+    const { data } = await api.get<PostResponseData>('posts', {
+      params: { per_page: 10, page },
+    });
+
+    setPosts(data.items);
+    setTotalPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    loadPosts();
+  }, [page]);
+
+  return (
+    <C.Flex flexDir="column">
+      <C.Text as="h2">Publicações</C.Text>
+      <C.Box mt="4" borderRadius="8" boxShadow="xs">
+        {posts.map(post => (
+          <C.Flex
+            key={post.id}
+            borderBottomWidth={1}
+            borderBottomColor="gray.200"
+            _last={{ border: 0 }}
+          >
+            <C.Flex p="4" width="100%">
+              <C.Text flex="1" isTruncated whiteSpace="normal" noOfLines={2}>
+                {post.title}
+              </C.Text>
+            </C.Flex>
+          </C.Flex>
+        ))}
+      </C.Box>
+
+      <Pagination
+        page={page}
+        total_pages={totalPages}
+        onChange={newPage => setPage(newPage)}
+      />
+    </C.Flex>
+  );
 };
 
 export default Posts;
+
+export const getServerSideProps = asLogged(async () => {
+  return {
+    props: {},
+  };
+}, [Roles.ADMIN, Roles.EDITOR]);
